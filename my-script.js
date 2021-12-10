b2205fabf51ef8850a968707a88a4b4ddcfcf91b
mahan <mahan.bayhaghi@gmail.com>
2021-12-10 15:36:11 +0330

    added send script and alert messages for send

--- my-script.js
+++ my-script.js
@@ -1,4 +1,10 @@
-function show_message(){
-	getElementsById(submit-btn);
-	console.log(4);
+
+function send() {
+	var r = confirm("Do you want to send this message ?");
+	if (r==true){
+		alert("Thank you for your feedback\n I'll do my best to answear quickly");
+	}
+	else{
+		alert("Message was aborted") ;
+	}
 }
